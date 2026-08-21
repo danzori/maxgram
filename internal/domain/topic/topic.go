268c26050700313c3a7c550c1a075f6c97516d15
danzori <main.danzori@gmail.com>
2026-08-22 01:04:05 +0300

    feat(domain): add topic model

---
+++ internal/domain/topic/topic.go
@@ -0,0 +1,27 @@
+package topic
+
+import (
+	"strings"
+	"time"
+	"unicode/utf8"
+)
+
+const MaxNameLength = 128
+
+type Topic struct {
+	ChatID   int64
+	ThreadID int64
+
+	Name      string
+	CreatedAt time.Time
+}
+
+func NormalizeName(raw string) string {
+	name := strings.Join(strings.Fields(raw), " ")
+
+	if utf8.RuneCountInString(name) > MaxNameLength {
+		name = string([]rune(name)[:MaxNameLength])
+	}
+
+	return name
+}
